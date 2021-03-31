0e36740d068a6222c1c422873c95028a62fbd477
Simon Lizotte <simon.lizotte.1@ulaval.ca>
2021-03-30 22:00:35 -0400

    add gaps

--- config.h
+++ config.h
@@ -2,6 +2,11 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const Bool showbar           = True;     /* 0 means no bar */
 static const Bool topbar            = True;     /* 0 means bottom bar */
