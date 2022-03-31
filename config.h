6f5af85388fc9b099ae5c46052a1baf771551492
Simon Lizotte <simon.lizotte.1@ulaval.ca>
2022-03-31 00:35:59 -0400

    add patch useless gaps

--- config.h
+++ config.h
@@ -3,6 +3,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const Bool showbar           = True;     /* 0 means no bar */
 static const Bool topbar            = True;     /* 0 means bottom bar */
