13b743e31c0d83dabee5ed4c023b416a6b32cd82
Simon Lizotte <simon.lizotte.1@ulaval.ca>
2023-03-06 19:47:31 -0500

    change terminal to xfce4-terminal, make style consistent, remove unused bindings

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const Bool showbar           = True;     /* 0 means no bar */
@@ -26,7 +26,7 @@ static const char *colors[][3]      = {
 	/*                 fg             bg           border   */
 	[SchemeNorm]   = { col_pale,      col_gray1,   col_gray1},
 	[SchemeSel]    = { col_gray1,     col_main,    col_main },
-        [SchemeDistro] = { col_constrast, col_gray1,   col_gray1 }
+	[SchemeDistro] = { col_constrast, col_gray1,   col_gray1 }
 };
 
 /* tagging */
@@ -38,9 +38,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class        instance    title         tags mask     isfloating   monitor */
-	{ "matplotlib", NULL,       NULL,         0,            1,           -1  },
-        { "Soffice",    "soffice",  NULL,         0,            0,           0,  },
-        { "Soffice",    "soffice",  "Presenting", 0,            0,           1,  }
+    { "matplotlib", NULL,       NULL,         0,            1,           -1  },
+    { "Soffice",    "soffice",  NULL,         0,            0,           0,  },
+    { "Soffice",    "soffice",  "Presenting", 0,            0,           1,  }
 };
 
 /* layout(s) */
@@ -70,8 +70,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_main, "-sf", col_gray4, NULL };
-static const char *roficmd[] = {"rofi", "-modi",  "run", "-show", "run", "-fake-transpacency"};
-static const char *termcmd[]  = { "urxvt", NULL };
+static const char *roficmd[] = { "rofi", "-modi",  "run", "-show", "run", "-fake-transpacency" };
+static const char *termcmd[]  = { "xfce4-terminal", "--hide-menubar" };
 
 static const char *mutecmd[] = { "amixer", "-D", "pulse", "sset", "Master", "toggle", NULL };
 static const char *volupcmd[] = { "amixer", "-D", "pulse", "sset", "Master", "5%+", NULL };
@@ -82,17 +82,18 @@ static const char *voldowncmd[] = { "amixer", "-D", "pulse", "sset", "Master", "
 #include "fullscreen.c"
 
 static Key keys[] = {
+	// Default commands
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_d,      spawn,          {.v = roficmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	//{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	//{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_z,      zoom,           {0} },
+	//{ MODKEY,                       XK_z,      zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
@@ -106,14 +107,14 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY,                       XK_period, tagmon,         {.i = +1 } },
-        // Additional commands
-        { MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	// Additional commands
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_f,      fullscreen,     {0} },
-        { 0,                     XF86XK_AudioMute, spawn,          {.v = mutecmd } },
-        { 0,                     XF86XK_AudioLowerVolume, spawn,   {.v = voldowncmd } },
-        { 0,                     XF86XK_AudioRaiseVolume, spawn,   {.v = volupcmd } },
+	{ 0,                            XF86XK_AudioMute, spawn,          {.v = mutecmd } },
+	{ 0,                            XF86XK_AudioLowerVolume, spawn,   {.v = voldowncmd } },
+	{ 0,                            XF86XK_AudioRaiseVolume, spawn,   {.v = volupcmd } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
